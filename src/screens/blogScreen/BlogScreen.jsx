408f8f606b701977f1a334b8ebbc41c9a3571098
Thomas Json <joogletom8@gmail.com>
2022-11-13 20:47:43 +0100

    Changes

--- src/screens/blogScreen/BlogScreen.jsx
+++ src/screens/blogScreen/BlogScreen.jsx
@@ -1,10 +1,16 @@
 import './blogScreen.scss';
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Article from '../../components/article/Article';
 
 const BlogScreen = () => {
     return (
         <main>
-            
+            <Container className='blog-container'>
+                <Article />
+                <Article />
+                <Article />
+            </Container>
         </main>
     );
 };
