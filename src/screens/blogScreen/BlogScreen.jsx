871850944f250bc9ae7dffbf4d29f6450a20abc8
Thomas Json <joogletom8@gmail.com>
2022-11-14 15:41:40 +0100

    Changes

--- src/screens/blogScreen/BlogScreen.jsx
+++ src/screens/blogScreen/BlogScreen.jsx
@@ -1,19 +1,34 @@
-import './blogScreen.scss';
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Article from '../../components/article/Article';
+import "./blogScreen.scss";
+import React from "react";
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import Article from "../../components/article/Article";
 
 const BlogScreen = () => {
-    return (
-        <main>
-            <Container className='blog-container'>
-                {/* // TODO Rajouter la category-nav */}
-                <Article />
-                <Article />
-                <Article />
-            </Container>
-        </main>
-    );
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://portfolio-api/article", {
+      method: "GET",
+    })
+      .then((resp) => resp.json())
+      .then((json) => {
+        setArticles(json);
+      });
+  }, []);
+
+  console.log(articles);
+
+  return (
+    <main>
+      <Container className="blog-container">
+        {/* // TODO Rajouter la category-nav */}
+        <Article />
+        <Article />
+        <Article />
+      </Container>
+    </main>
+  );
 };
 
 export default BlogScreen;
