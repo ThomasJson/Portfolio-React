15f9c8175256d517882e45cd674f782771f553dc
Thomas Json <joogletom8@gmail.com>
2022-11-25 13:38:49 +0100

    Changes

--- src/screens/blogScreen/BlogScreen.jsx
+++ src/screens/blogScreen/BlogScreen.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Article from "../../components/article/Article";
 import useFetch from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const BlogScreen = () => {
   const navigate = useNavigate();
@@ -22,25 +23,28 @@ const BlogScreen = () => {
 
   return (
     <main>
-      {data &&
-        data?.data.map((article) => {
-          return (
-            <div
-              key={article.Id_article}
-              onClick={() => {
-                navigate(`/article/${article.Id_article}`);
-              }}
-            >
-              <Article
-                title={article.title}
-                content={article.content}
-                src={article.with[0]?.src}
-                alt={article.with[0]?.alt}
-                category={article.with[1]?.title}
-              />
-            </div>
-          );
-        })}
+      <Container fluid className="blog-container">
+        {data &&
+          data?.data.map((article) => {
+            return (
+              <div
+                key={article.Id_article}
+                onClick={() => {
+                  navigate(`/article/${article.Id_article}`);
+                }}
+              >
+                <Article
+                  title={article.title}
+                  content={article.content}
+                  src={article.with[0]?.src}
+                  alt={article.with[0]?.alt}
+                  category={article.with[1]?.title}
+                  color={article.with[1]?.title}
+                />
+              </div>
+            );
+          })}
+      </Container>
     </main>
   );
 };
