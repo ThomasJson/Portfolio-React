87fb3f82ef8143d7be652ebebed3113cc5c1dbb7
Thomas Json <joogletom8@gmail.com>
2022-12-05 16:44:41 +0100

    Changes

--- src/screens/contactScreen/ContactScreen.jsx
+++ src/screens/contactScreen/ContactScreen.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import { Button, Container } from "react-bootstrap";
 import doFetch from "../../helpers/fetchHelper";
+import DisplayMap from "../../components/displayMap/DisplayMap";
 
 const ContactScreen = () => {
   const handleSubmit = async (e) => {
@@ -27,31 +28,51 @@ const ContactScreen = () => {
 
   return (
     <main>
-      <Container fluid className="contact-bloc">
-        <Form className="contact-form" onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Control type="text" placeholder="Name" id="name-contact" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Control type="email" placeholder="Email" id="email-contact" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Control
-              type="text"
-              placeholder="Subject"
-              id="subject-contact"
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Control
-              as="textarea"
-              placeholder="Message"
-              rows={6}
-              id="message-contact"
-            />
-          </Form.Group>
-          <Button type="submit">Envoyer</Button>
-        </Form>
+      <Container fluid>
+        <Container fluid className="contact-bloc">
+          <Container fluid className="contact-section">
+            <h2>Afin de me contacter</h2>
+            <p>
+              Pour un premier contact professionel ou pour partager votre avis
+              sur un article 😉
+            </p>
+            <Form className="contact-form" onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Name"
+                  id="name-contact"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  id="email-contact"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Subject"
+                  id="subject-contact"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  as="textarea"
+                  placeholder="Message"
+                  rows={7}
+                  id="message-contact"
+                />
+              </Form.Group>
+              <Button className="btn-login" type="submit">
+                Envoyer
+              </Button>
+            </Form>
+          </Container>
+          <DisplayMap />
+        </Container>
       </Container>
     </main>
   );
