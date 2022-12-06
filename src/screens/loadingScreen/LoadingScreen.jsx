49d0621756ec9a085da81874e69d13ee0876155f
Thomas Json <joogletom8@gmail.com>
2022-12-06 22:50:17 +0100

    Changes

--- src/screens/loadingScreen/LoadingScreen.jsx
+++ src/screens/loadingScreen/LoadingScreen.jsx
@@ -1,12 +1,15 @@
-import './loadingScreen.scss';
-import React from 'react';
+import "./loadingScreen.scss";
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const LoadingScreen = () => {
-    return (
-        <div>
-            LOADING
-        </div>
-    );
+  return (
+    <main>
+      <Container fluid className="center-bloc">
+        LOADING
+      </Container>
+    </main>
+  );
 };
 
 export default LoadingScreen;
